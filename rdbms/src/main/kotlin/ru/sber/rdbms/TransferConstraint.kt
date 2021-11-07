72637c5865655c9e5241f68aed9d352be2e6d2b3
Anton Fedorov <avfedorov235@gmail.com>
2021-11-07 20:54:22 +0300

    +rdbms

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferConstraint.kt
@@ -1,7 +1,35 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+import java.sql.SQLException
+
 class TransferConstraint {
+
+    private val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/db",
+        "postgres",
+        "postgres"
+    )
+
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+
+        connection.use { connection ->
+            try {
+                connection.prepareStatement("UPDATE account1 SET amount = amount - ? WHERE id = ?").use { prepared ->
+                    prepared.setLong(1, amount)
+                    prepared.setLong(2, accountId1)
+                    prepared.executeUpdate()
+                }
+
+                connection.prepareStatement("UPDATE account1 SET amount = amount + ? WHERE id = ?").use { prepared ->
+                    prepared.setLong(1, amount)
+                    prepared.setLong(2, accountId2)
+                    prepared.executeUpdate()
+                }
+
+            } catch (exception: SQLException) {
+                println(exception.message)
+            }
+        }
     }
 }
--- rdbms/src/main/resources/init.sql
+++ rdbms/src/main/resources/init.sql
@@ -2,11 +2,13 @@
 
 --changeset rrmasgutov:init
 
+drop table if exists account1;
+
 create table account1
 (
     id bigserial constraint account_pk primary key,
-    amount int,
-    version int
+    amount int check (amount >= 0),
+    version int default 0
 );
 
 
